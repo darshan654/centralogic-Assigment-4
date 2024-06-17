96018ed87190eeb77fd251c01d40d40cb1e31b6c
darshan <sudthakare73@gmail.com>
2024-06-17 13:34:45 +0530

    removed skills

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -1,64 +1,34 @@
-import React from 'react'
+import React from 'react';
 
-export default function SkillsSection() {
+const SkillsSection = () => {
     return (
         <section id="skills" className="w-full p-8">
             <h2 className="text-2xl font-bold mb-4 text-white">What I Know</h2>
             <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-                
-                <div className="flex flex-col items-center">
-                    <img src="https://cdn.svgporn.com/logos/html-5.svg" alt="HTML5" className="w-16 h-16" />
-                    <p>HTML5</p>
-                </div>
-                <div className="flex flex-col items-center">
-                    <img src="https://cdn.svgporn.com/logos/css-3.svg" alt="CSS3" className="w-16 h-16" />
-                    <p>CSS3</p>
-                </div>
-                <div className="flex flex-col items-center">
-                    <img src="https://cdn.svgporn.com/logos/javascript.svg" alt="JavaScript" className="w-16 h-16" />
-                    <p>JavaScript</p>
-                </div>
-                <div className="flex flex-col items-center">
-                    <img src="https://cdn.svgporn.com/logos/react.svg" alt="React" className="w-16 h-16" />
-                    <p>React Js</p>
-                </div>
-                <div className="flex flex-col items-center">
-                    <img src="https://cdn.svgporn.com/logos/bootstrap.svg" alt="Bootstrap" className="w-16 h-16" />
-                    <p>Bootstrap</p>
-                </div>
-                <div className="flex flex-col items-center">
-                    <img src="https://cdn.svgporn.com/logos/tailwindcss-icon.svg" alt="Tailwind" className="w-16 h-16" />
-                    <p>Tailwind</p>
-                </div>
-                <div className="flex flex-col items-center">
-                    <img src="https://flowbite.s3.amazonaws.com/brand/logo-dark/mark/flowbite-logo.png" alt="Flowbite UI" className="w-16 h-16" />
-                    <p>Flowbite UI</p>
-                </div>
-                <div className="flex flex-col items-center">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMeksyLCg8_a1aWg_J3E9E9_NZAGL5HSHoXA&s" alt="Shards UI" className="w-16 h-16" />
-                    <p>Shards UI</p>
-                </div>
-                <div className="flex flex-col items-center">
-                    <img src="https://cdn.svgporn.com/logos/ant-design.svg" alt="Ant Design" className="w-16 h-16" />
-                    <p>Ant Design</p>
-                </div>
-                {/* <div className="flex flex-col items-center">
-                    <img src="https://cdn.svgporn.com/logos/mysql.svg" alt="MySQL" className="w-16 h-16" />
-                    <p>MySQL</p>
-                </div> */}
-                <div className="flex flex-col items-center">
-                    <img src="https://cdn.svgporn.com/logos/mongodb.svg" alt="MongoDB" className="w-16 h-16" />
-                    <p>MongoDB</p>
-                </div>
-
-                <div className="flex flex-col items-center">
-                    <img src="https://cdn.svgporn.com/logos/visual-studio-code.svg" alt="Visual Studio Code" className="w-16 h-16" />
-                    <p>Visual Studio Code</p>
-                </div>
-                <div className="flex flex-col items-center">
-                    <img src="https://cdn.svgporn.com/logos/git-icon.svg" alt="Git & GitHub" className="w-16 h-16" />
-                    <p>Git & GitHub</p>
-                </div>
+                <SkillCard imageSrc="https://cdn.svgporn.com/logos/html-5.svg" skillName="HTML5" />
+                <SkillCard imageSrc="https://cdn.svgporn.com/logos/css-3.svg" skillName="CSS3" />
+                <SkillCard imageSrc="https://cdn.svgporn.com/logos/javascript.svg" skillName="JavaScript" />
+                <SkillCard imageSrc="https://cdn.svgporn.com/logos/react.svg" skillName="React Js" />
+                <SkillCard imageSrc="https://cdn.svgporn.com/logos/bootstrap.svg" skillName="Bootstrap" />
+                <SkillCard imageSrc="https://cdn.svgporn.com/logos/tailwindcss-icon.svg" skillName="Tailwind" />
+                <SkillCard imageSrc="https://flowbite.s3.amazonaws.com/brand/logo-dark/mark/flowbite-logo.png" skillName="Flowbite UI" />
+                <SkillCard imageSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMeksyLCg8_a1aWg_J3E9E9_NZAGL5HSHoXA&s" skillName="Shards UI" />
+                <SkillCard imageSrc="https://cdn.svgporn.com/logos/ant-design.svg" skillName="Ant Design" />
+                <SkillCard imageSrc="https://cdn.svgporn.com/logos/mongodb.svg" skillName="MongoDB" />
+                <SkillCard imageSrc="https://cdn.svgporn.com/logos/visual-studio-code.svg" skillName="Visual Studio Code" />
+                <SkillCard imageSrc="https://cdn.svgporn.com/logos/git-icon.svg" skillName="Git & GitHub" />
             </div>
-        </section>)
-}
+        </section>
+    );
+};
+
+const SkillCard = ({ imageSrc, skillName }) => {
+    return (
+        <div className="flex flex-col items-center transform hover:scale-105 transition-transform duration-300 ease-in-out">
+            <img src={imageSrc} alt={skillName} className="w-16 h-16 mb-2 rounded-lg shadow-lg hover:shadow-xl" />
+            <p className="text-sm text-gray-300">{skillName}</p>
+        </div>
+    );
+};
+
+export default SkillsSection;
