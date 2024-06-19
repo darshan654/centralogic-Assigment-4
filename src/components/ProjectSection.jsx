1cdc514df2bc2e7a18e5d5fb80fb507457b55fef
darshan <sudthakare73@gmail.com>
2024-06-20 00:34:34 +0530

    movie project added

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -4,6 +4,7 @@ import currency_converter from '/public/assets/project//currency converter.png'
 import Expense_Tracker from '/public/assets/project/Expense Tracker.png'
 import todolist from '/public/assets/project/todolist.png'
 import upcoming from '/public/assets/project/upcoming.png'
+import Movie from '/public/assets/project/Movie-search-Web-App.png'
 
 const ProjectSection = () => {
     return (
@@ -14,32 +15,40 @@ const ProjectSection = () => {
             <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14 mx-auto max-w-7xl">
 
                 {/*Ecommerce web front-end*/}
-                <ProjectCard 
+                <ProjectCard
                     imageSrc={ecommerce}
                     title="Ecommerce web front-end"
                     description="A user-friendly and responsive front-end interface for an ecommerce platform enabling seamless browsing, shopping, and checkout experiences."
                     netlifyLink="https://ecommerce-front-end-demo.netlify.app/"  // Replace with actual Netlify link when ready
                     githubLink="https://github.com/darshan654/centralogic-Assigment-5"  // Replace with actual GitHub link
                 />
+                {/*Move-search-Web-App*/}
+                <ProjectCard
+                    imageSrc={Movie}
+                    title="Movie-Search-Web-App"
+                    description="A React-based web application that allows users to search for movies and view their details using the OMDB API."
+                    netlifyLink="https://moviesearch-webapp.netlify.app/"  // Replace with actual Netlify link when ready
+                    githubLink="https://github.com/darshan654/Move-search-Web-App"  // Replace with actual GitHub link
+                />
                 {/*Ecommerce */}
-                <ProjectCard 
+                <ProjectCard
                     imageSrc={upcoming}
                     title="Up Coming Ecommerce "
-                    
+
                     description="A full-stack MERN (MongoDB, Express.js, React.js, Node.js) ecommerce platform under development."
-                      // Replace with actual Netlify link when ready
-                      // Replace with actual GitHub link
+                    // Replace with actual Netlify link when ready
+                    // Replace with actual GitHub link
                     customClass="opacity-50"
                 />
 
-                
-                </div>
-                
-              
-                <div className="py-6 lg:py-12 font-titleFont flex flex-col gap-4 mx-auto max-w-7xl">
-                         <h2 className="text-3xl md:text-4xl font-bold text-white text-center mb-4 mt-8">Mini Projects</h2>
-                </div>
-                <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14 mx-auto max-w-7xl">
+
+            </div>
+
+
+            <div className="py-6 lg:py-12 font-titleFont flex flex-col gap-4 mx-auto max-w-7xl">
+                <h2 className="text-3xl md:text-4xl font-bold text-white text-center mb-4 mt-8">Mini Projects</h2>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14 mx-auto max-w-7xl">
 
                 {/*Expense Tracker*/}
                 <ProjectCard
@@ -86,7 +95,7 @@ const ProjectCard = ({ imageSrc, title, description, netlifyLink, githubLink, cu
                 <div className="flex justify-center mt-4">
                     <a href={githubLink} target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-gray-200">
                         <svg className="w-8 h-8 md:w-10 md:h-10 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path fillRule="evenodd" clipRule="evenodd" d="M12 2C6.48 2 2 6.48 2 12c0 4.42 2.865 8.17 6.84 9.49.5.09.68-.22.68-.48l-.01-1.68c-2.782.6-3.37-1.34-3.37-1.34-.455-1.15-1.112-1.46-1.112-1.46-.91-.62.07-.61.07-.61 1.006.07 1.532 1.03 1.532 1.03.893 1.53 2.347 1.09 2.92.83.09-.65.35-1.09.64-1.34-2.24-.25-4.588-1.12-4.588-5.01 0-1.11.4-2.01 1.03-2.72-.1-.25-.45-1.287.1-2.675 0 0 .85-.27 2.775 1.04.8-.22 1.65-.33 2.5-.33.85 0 1.7.11 2.5.33 1.92-1.31 2.77-1.04 2.77-1.04.56 1.39.21 2.425.1 2.675.64.71 1.03 1.61 1.03 2.72 0 3.9-2.35 4.76-4.6 5.01.36.31.68.92.68 1.86l-.01 2.77c0 .26.18.57.69.47A10.013 10.013 0 0 0 22 12c0-5.52-4.48-10-10-10Z"/>
+                            <path fillRule="evenodd" clipRule="evenodd" d="M12 2C6.48 2 2 6.48 2 12c0 4.42 2.865 8.17 6.84 9.49.5.09.68-.22.68-.48l-.01-1.68c-2.782.6-3.37-1.34-3.37-1.34-.455-1.15-1.112-1.46-1.112-1.46-.91-.62.07-.61.07-.61 1.006.07 1.532 1.03 1.532 1.03.893 1.53 2.347 1.09 2.92.83.09-.65.35-1.09.64-1.34-2.24-.25-4.588-1.12-4.588-5.01 0-1.11.4-2.01 1.03-2.72-.1-.25-.45-1.287.1-2.675 0 0 .85-.27 2.775 1.04.8-.22 1.65-.33 2.5-.33.85 0 1.7.11 2.5.33 1.92-1.31 2.77-1.04 2.77-1.04.56 1.39.21 2.425.1 2.675.64.71 1.03 1.61 1.03 2.72 0 3.9-2.35 4.76-4.6 5.01.36.31.68.92.68 1.86l-.01 2.77c0 .26.18.57.69.47A10.013 10.013 0 0 0 22 12c0-5.52-4.48-10-10-10Z" />
                         </svg>
                     </a>
                 </div>
